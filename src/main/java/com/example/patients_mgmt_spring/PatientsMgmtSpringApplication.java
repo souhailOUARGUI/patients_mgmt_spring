f9de2a3cae1057588c40a789cdfd0f569081dba8
souhailOUARGUI <ouargui550@gmail.com>
2025-03-17 11:34:58 +0000

    first commit

---
+++ src/main/java/com/example/patients_mgmt_spring/PatientsMgmtSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.patients_mgmt_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsMgmtSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsMgmtSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/patients_mgmt_spring/PatientsMgmtSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patients_mgmt_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsMgmtSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
