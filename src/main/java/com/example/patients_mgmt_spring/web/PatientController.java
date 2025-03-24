6fba1bbaabd49941cda623cf280a7923dad43fe4
souhailOUARGUI <ouargui550@gmail.com>
2025-03-24 11:19:43 +0000

    added the bootstrap dependency to patients.html

--- src/main/java/com/example/patients_mgmt_spring/web/PatientController.java
+++ src/main/java/com/example/patients_mgmt_spring/web/PatientController.java
@@ -1,9 +1,13 @@
 package com.example.patients_mgmt_spring.web;
 
+
+import com.example.patients_mgmt_spring.entities.Patient;
 import com.example.patients_mgmt_spring.repository.PatientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -11,7 +15,9 @@ public class PatientController {
 
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model){
+        List<Patient> patientList  = patientRepository.findAll();
+        model.addAttribute("listPatients",patientList);
         return "patients";
     }
 
