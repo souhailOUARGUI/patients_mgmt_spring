bb9e4f29a60907fcdb5a2b15e7ab18bab8165796
souhailOUARGUI <ouargui550@gmail.com>
2025-04-20 22:41:45 +0100

    fetched patients with pagination (fetch only 4 patients)

--- src/main/java/com/example/patients_mgmt_spring/web/PatientController.java
+++ src/main/java/com/example/patients_mgmt_spring/web/PatientController.java
@@ -4,6 +4,8 @@ package com.example.patients_mgmt_spring.web;
 import com.example.patients_mgmt_spring.entities.Patient;
 import com.example.patients_mgmt_spring.repository.PatientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,8 @@ public class PatientController {
     private PatientRepository patientRepository;
     @GetMapping("/index")
     public String index(Model model){
-        List<Patient> patientList  = patientRepository.findAll();
-        model.addAttribute("listPatients",patientList);
+        Page<Patient> patientPage  = patientRepository.findAll(PageRequest.of(0,4));
+        model.addAttribute("listPatients",patientPage.getContent());
         return "patients";
     }
 
